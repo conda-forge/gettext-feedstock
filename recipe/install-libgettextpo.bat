90670078c4e194e27e57b45b23fda728d3c4fb6d
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-18 19:20:16 +0100

    Create if not exists

--- recipe/install-libgettextpo.bat
+++ recipe/install-libgettextpo.bat
@@ -1,6 +1,6 @@
 @echo on
 
-md %LIBRARY_PREFIX%\bin
+if not exist %LIBRARY_PREFIX%\bin md %LIBRARY_PREFIX%\bin
 if errorlevel 1 exit 1
 
 copy gettext-tools\libgettextpo\.libs\gettextpo-0.dll %LIBRARY_PREFIX%\bin
