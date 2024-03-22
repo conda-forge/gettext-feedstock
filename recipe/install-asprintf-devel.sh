0104a0e1c11da8d8067ca84c7434cec4f357614f
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-22 11:22:15 +0100

    Fill devel packages on unix

--- recipe/install-asprintf-devel.sh
+++ recipe/install-asprintf-devel.sh
@@ -2,5 +2,14 @@
 
 set -euxo pipefail
 
-exit 1
+mkdir -p ${PREFIX}/include
+cp ./gettext-runtime/libasprintf/autosprintf.h ${PREFIX}/include/autosprintf.h
+
+pushd ${PREFIX}/lib
+if [[ "${target_platform}" == osx-* ]]; then
+  ln -s libasprintf.*.dylib libasprintf.dylib
+else
+  ln -s libasprintf.so.* libasprintf.so
+fi
+popd
 
--- recipe/install-libintl-devel.sh
+++ recipe/install-libintl-devel.sh
@@ -2,4 +2,13 @@
 
 set -euxo pipefail
 
-exit 1
+mkdir -p ${PREFIX}/include
+cp ./gettext-runtime/intl/libintl.h ${PREFIX}/include/libintl.h
+
+pushd ${PREFIX}/lib
+if [[ "${target_platform}" == osx-* ]]; then
+  ln -s libintl.*.dylib libintl.dylib
+else
+  ln -s libintl.so.* libintl.so
+fi
+popd
