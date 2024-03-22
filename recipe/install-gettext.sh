644f528fa526d2b1c12c562011bfc628fce8d31b
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-22 09:56:25 +0100

    Add devel subpackages

--- recipe/install-gettext.sh
+++ recipe/install-gettext.sh
@@ -4,6 +4,13 @@ set -euxo pipefail
 
 make install
 
+if [[ "${PKG_NAME}" != "gettext" ]]; then
+  rm -rf ${PREFIX}/share/gettext/projects
+  rm -rf ${PREFIX}/share/doc/libtextstyle
+  rm -rf ${PREFIX}/share/doc/gettext/examples
+  rm -rf ${PREFIX}/share/doc/gettext/javadoc2
+fi
+
 # This overlaps with readline:
 rm -rf ${PREFIX}/share/info/dir
 
