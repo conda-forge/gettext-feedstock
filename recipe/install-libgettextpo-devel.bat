e63903039a03728bef4c9b73a04af6d6012c874c
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-25 17:36:26 +0000

    Fix gettextpo.lib name

--- recipe/install-libgettextpo-devel.bat
+++ recipe/install-libgettextpo-devel.bat
@@ -9,11 +9,10 @@ if errorlevel 1 exit 1
 if not exist %LIBRARY_PREFIX%\lib md %LIBRARY_PREFIX%\lib
 if errorlevel 1 exit 1
 
-dir gettext-tools\libgettextpo\.libs
-copy gettext-tools\libgettextpo\.libs\gettextpo-0.dll.lib %LIBRARY_PREFIX%\lib
+copy gettext-tools\libgettextpo\.libs\gettextpo.dll.lib %LIBRARY_PREFIX%\lib
 if errorlevel 1 exit 1
 
 @rem Enforce dynamic linkage
-copy gettext-tools\libgettextpo\.libs\gettextpo-0.dll.lib %LIBRARY_PREFIX%\lib\gettextpo-0.lib
+copy gettext-tools\libgettextpo\.libs\gettextpo.dll.lib %LIBRARY_PREFIX%\lib\gettextpo.lib
 if errorlevel 1 exit 1
 
